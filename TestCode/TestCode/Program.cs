58bbcfc11ca9e3a3dfd1167922890db4d528f2c1
Eamon <Chenlulei@live.com>
2015-09-18 16:50:42 +0800

    add code conteet

--- TestCode/TestCode/Program.cs
+++ TestCode/TestCode/Program.cs
@@ -6,10 +6,59 @@ using System.Threading.Tasks;
 
 namespace TestCode
 {
+
+
+    public enum Animal
+    {
+        Dog = 1,
+        Cat,
+        Bird
+
+    }
+
+    public class AnimalTypeAttribute : Attribute
+    {
+        public AnimalTypeAttribute(Animal pet)
+        {
+
+            DefaultPet = pet;
+        }
+        public Animal DefaultPet { get; set; }
+    }
+
+    public class AnimalTypeTessClass
+    {
+        [AnimalType(Animal.Dog)]
+        public void Dog() { }
+
+        [AnimalType(Animal.Cat)]
+        public void Cat() { }
+
+        [AnimalType(Animal.Bird)]
+        public void Bird() { }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            var testclass = new AnimalTypeTessClass();
+            var type = testclass.GetType();
+
+
+            foreach (var info in type.GetMethods())
+            {
+                foreach (var attr in Attribute.GetCustomAttributes(info).Where(attr => attr.GetType() == typeof(AnimalTypeAttribute)))
+                {
+                    Console.WriteLine("Method {0} has a pet {1} attribute.", info.Name, ((AnimalTypeAttribute)attr).DefaultPet);
+                }
+
+            }
+            Console.Read();
         }
+
+
     }
 }
