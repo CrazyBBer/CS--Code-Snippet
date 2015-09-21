f5f6a269a96ad19bcd329d3f643afab1aad05982
Eamon <Chenlulei@live.com>
2015-09-21 15:14:11 +0800

    add getAssemblyguid()

--- TestCode/NoticeMessageLoop/Program.cs
+++ TestCode/NoticeMessageLoop/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,16 @@ namespace NoticeMessageLoop
     {
         static void Main(string[] args)
         {
+
+        }
+
+
+        static string getAssemblyguid()
+        {
+
+            var guitattr =
+                (GuidAttribute)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(GuidAttribute));
+            return string.Format("DemoSet-{0}", guitattr.Value);
         }
     }
 }
